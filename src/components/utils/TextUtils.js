344e0746528ce987a8848f87b1a0d9349793773c
DictaMed <akio963@gmail.com>
2025-11-07 19:22:43 +0100

    h

---
+++ src/components/utils/TextUtils.js
@@ -0,0 +1,6 @@
+export const PATIENT_INFO = "Informations du patient";
+export const FILE_NUMBER = "Numéro de dossier";
+export const NAME = "nom & prénom";
+export const BMI_INSTRUCTION = "vous pouvez dire : Pas de tabac, patient non tabagique, tabac non.";
+export const DYSLIPIDEMIA = "DYSLIPIDEMIE";
+export const BLOOD_TEST = "Hémoglobine, globules blancs, plaquettes, ionogramme complet( sodium , potassium, chlore)";
